7f9ee4893f4bfe4c6942bf6624985e86ec128776
Peter Bourget <pbourget@w6op.com>
2020-11-26 11:24:43 -0800

    Initial Commit

---
+++ xSpaceWeather/xSpaceWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  xSpaceWeatherApp.swift
+//  xSpaceWeather
+//
+//  Created by Peter Bourget on 11/26/20.
+//
+
+import SwiftUI
+
+@main
+struct xSpaceWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
